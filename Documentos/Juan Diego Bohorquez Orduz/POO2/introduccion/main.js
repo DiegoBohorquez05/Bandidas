2cfc2c16f881f199769fe275b4c8f397d0909f7b
Daniela Salazar <slazardaniela05@gmail.com>
2023-02-15 09:09:02 -0500

    terminado

--- Documentos/Juan Diego Bohorquez Orduz/POO2/introduccion/main.js
+++ Documentos/Juan Diego Bohorquez Orduz/POO2/introduccion/main.js
@@ -14,62 +14,94 @@
 
 // console.log(cualquierNombre)
 
-class Persona{
-    name
-    last_name
-    age
-    bandidas
+const datos = [];
+let cari = [];
 
-    constructor(name,last_name,age,bandidas=''){
-        this.name=name
-        this.last_name=last_name
-        this.age=age
-        this.bandidas=bandidas
-    }
+class Persona {
+  name;
+  last_name;
+  age;
+  bandidas;
 
-    eliminarBandida(nombreBandidas){
-        const bandidasSerias=this.cari.filter(bandida =>{
-            return bandida != nombreBandidas
-        })
-        this.cari=bandidasSerias
-    }
+  constructor(name, last_name, age, bandidas = []) {
+    this.name = name;
+    this.last_name = last_name;
+    this.age = age;
+    this.bandidas = bandidas;
+  }
+
+  eliminarBandida(nombreBandidas) {
+    const bandidasSerias = this.bandidas.filter((bandida) => {
+      return bandida != nombreBandidas;
+    });
+    this.bandidas = bandidasSerias;
+  }
+
+  agregarBandida(nombreBandidas) {
+        this.bandidas.push(nombreBandidas)
+  }
 }
 
-const guardar=document.getElementById('boton1')
-const bandidas=document.getElementById('boton2')
-const eliminar=document.getElementById('boton3')
+const guardar = document.getElementById("boton1");
+const bandidas = document.getElementById("boton2");
+const eliminar = document.getElementById("boton3");
 
-const datos=[]
-let cari=[]
+bandidas.addEventListener("click", () => {
+  //   let bandiName = document.getElementById("name2").value;
+  //   cari.push(bandiName);
+  //   console.log(cari);
+  //   bandiName = document.getElementById("name2").value = "";
 
-bandidas.addEventListener('click',()=>{
-    let bandiName=document.getElementById('name2').value
-    cari.push(bandiName)
-    console.log(cari)
-    bandiName=document.getElementById('name2').value=""
+  //   const listaBandidas = document.getElementById("listaBandidas");
+  //   listaBandidas.innerHTML = `Tus bandidas son: ${cari}`;
+  let persona;
+  let nombreBandidas = document.getElementById("name2").value;
+  const name = document.getElementById("nombres").value;
 
-    const listaBandidas=document.getElementById('listaBandidas')
-    listaBandidas.innerHTML=
-    `Tus bandidas son: ${cari}`
-})
+  for(let i=0;i<datos.length;i++){
+    if(datos[i].name==name){
+        persona=datos[i]
+        console.log(persona)
+        persona.agregarBandida(nombreBandidas);
+        // cari.push(nombreBandidas);
+        console.log(persona)
+    }
+    const listaBandidas = document.getElementById("listaBandidas");
+    listaBandidas.innerHTML = `Tus bandidas son: ${cari}`;
+  }
+});
 
+guardar.addEventListener("click", () => {
+  let name = document.getElementById("nombres").value;
+  let apellidos = document.getElementById("apellidos").value;
+  let edad = document.getElementById("edad").value;
 
-guardar.addEventListener('click',()=>{
-    const name=document.getElementById('nombres').value
-    const apellidos=document.getElementById('apellidos').value
-    const edad=document.getElementById('edad').value
+  const nuevaPersona = new Persona(name, apellidos, edad, cari);
+  datos.push(nuevaPersona);
 
-    const nuevaPersona=new Persona(name,apellidos,edad,cari)
-    datos.push(nuevaPersona,cari)
+  console.log(nuevaPersona);
+  console.log(datos);
+  cari = [];
+  name = document.getElementById("nombres").value = "";
+  apellidos = document.getElementById("apellidos").value = "";
+  edad = document.getElementById("edad").value = "";
+});
 
-    console.log(nuevaPersona)
-    console.log(datos)
-    cari=[]
-})
+eliminar.addEventListener("click", () => {
+  let persona;
+  let nombreBandidas = document.getElementById("name2").value;
+  const name = document.getElementById("nombres").value;
 
+  for (let i = 0; i < datos.length; i++) {
+    if (datos[i].name == name) {
+      persona = datos[i];
+      console.log(persona);
+      persona.eliminarBandida(nombreBandidas);
+      console.log(persona);
+    }
+  }
 
-eliminar.addEventListener('click',()=>{
-    let nombreBandidas=document.getElementById('name2')
-    this.eliminarBandida(nombreBandidas)
-    console.log(bandidasSerias)
-})
+  //   const bandido = datos.find((persona) => {
+  //     return persona.nombre === name;
+  //   });
+});
